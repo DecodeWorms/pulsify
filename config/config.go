244160a5ae465f7e53f2e7541749ee12ff03fa0b
abdulhmeed <lekzy.csharpl@gmail.com>
2024-11-17 14:11:24 +0100

    refactor:updated utils and config file

--- config/config.go
+++ config/config.go
@@ -1,21 +1,82 @@
 package config
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	appEnv      = "APP_ENV"
+	servicePort = "APP_PORT"
+	pulsarUrl   = "PULSAR_URL"
+)
+
+type source interface {
+	GetEnv(key string, fallback string) string
+	GetEnvBool(key string, fallback bool) bool
+	GetEnvInt(key string, fallback int) int
+}
+
+type OSSource struct {
+	source //nolint
+}
+
+func (o OSSource) GetEnv(key string, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
+
+func (o OSSource) GetEnvBool(key string, fallback bool) bool {
+	b := o.GetEnv(key, "")
+	if len(b) == 0 {
+		return fallback
+	}
+	v, err := strconv.ParseBool(b)
+	if err != nil {
+		return fallback
+	}
+	return v
+}
+
+func (o OSSource) GetEnvInt(key string, fallback int) int {
+	if value, exists := os.LookupEnv(key); exists {
+		result, err := strconv.Atoi(value)
+		if err != nil {
+			return fallback
+		}
+		return result
+	}
+	return fallback
+}
 
 type Config struct {
-	Url            string
-	Authentication bool
-	Token          string
+	AppEnv      string
+	ServicePort string
+	Host        string
+	Port        string
+	PulsarUrl   string
 }
 
-func NewConfig(url, token string, auth bool) Config {
-	return Config{
-		Url:            url,
-		Authentication: auth,
-		Token:          token,
+func ImportConfig(source source) Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
-}
 
-func (c *Config) String() string {
-	return fmt.Sprintf("URL: %s, Auth: %v", c.Url, c.Authentication)
+	appEnv := source.GetEnv(appEnv, "")
+	port := source.GetEnv(servicePort, "8001")
+	url := source.GetEnv(pulsarUrl, "")
+	smtpPort := source.GetEnv(port, "")
+
+	return Config{
+		AppEnv:      appEnv,
+		ServicePort: port,
+		PulsarUrl:   url,
+		Port:        smtpPort,
+	}
 }
